463a6f7297f26641583bb3efad3bd1c9b2454968
thomazrequena <thomazrequena@gmail.com>
2025-09-08 17:05:02 -0300

    Read crop

--- Cap 1 - FarmTech na era da cloud computing.py
+++ Cap 1 - FarmTech na era da cloud computing.py
@@ -1,13 +1,2 @@
-# Imports
 import pandas as pd
-
-import numpy as np
-from pandas import DataFrame
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
-# Ler o dataset
-
-dataset = pd.read_csv('crop_yield.csv')
+df = pd.read_csv("crop_yield.csv")
